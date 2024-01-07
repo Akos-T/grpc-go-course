776da3f47e27b4a601a2a774b537cf3563d57384
Akos-T <akosh.toth@gmail.com>
2024-01-07 20:19:03 +0100

    feat(interceptor): Add example

--- greet/server/main.go
+++ greet/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -25,9 +26,14 @@ func main() {
 
 	log.Printf("Listening on %s'n", addr)
 
-	opts := []grpc.ServerOption{}
+	opts := []grpc.ServerOption{
+		// The following grpc.ServerOption adds an interceptor for all unary RPCs. To configure an interceptor
+		// for streaming RPCs, see: https://godoc.org/google.golang.org/grpc#StreamInterceptor
+		grpc.UnaryInterceptor(exampleInterceptor),
+	}
 	tls := true // Change it to false if you want to run without TLS
 	if tls {
+		// Enable TLS for all incoming connections.
 		certFile := "../ssl/server.crt"
 		keyFile := "../ssl/server.pem"
 		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
@@ -43,5 +49,15 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+}
 
+func exampleInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+	log.Println(" --- This is the UNARY interceptor before running the actual operation! We can do authentication/authorization here for example and stop if necessary.")
+
+	// Continue execution of handler
+	res, err := handler(ctx, req)
+
+	log.Println(" --- This is the UNARY interceptor after running the actual operation! We can manipulate the response here before sending it back.")
+
+	return res, err
 }
